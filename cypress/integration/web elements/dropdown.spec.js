74a266a48a3a4b10cb8244645af983740a303b33
hungdaoqc <hungd@criticalmass.com>
2019-03-21 18:29:48 +0700

    add new drop-down

--- cypress/integration/web elements/dropdown.spec.js
+++ cypress/integration/web elements/dropdown.spec.js
@@ -1,20 +1,37 @@
 require('cypress-xpath')
 
 describe('Handle drop-down menu', () => {
-    beforeEach(() => {
-        cy.visit('https://www.vietjetair.com')
+    context('Drop-down without navigation', () => {
+        
+        beforeEach(() => {
+            cy.visit('https://www.vietjetair.com')
+        })
+    
+        it('Dynamic dropdown by CSS', () => {
+    
+            cy.get('.selection').contains('Điểm khởi hành')
+                .click()
+            cy.get('.select2-results__option').contains('Tp. Hồ Chí Minh (SGN)')
+                .click()
+    
+        })
+        it('Dynamic dropdown by Xpath', () => {
+            cy.xpath(".//span[@title='Điểm khởi hành']").click()
+            cy.xpath('//li[text()="Cần Thơ (VCA)"]').click()
+        })
     })
 
-    it('Dynamic dropdown by CSS', () => {
+    context('Drop-down with navigation', () =>{
+        it.only('Check drop-down with navigation', ()=>{
+            cy.visit('https://www.spicejet.com/')
+            cy.get('.adult-infant-child').within(() => {
+                cy.get('#divpaxinfo').click()
+                    // choose number of adult
+                    cy.get('#divAdult #hrefIncAdt').dblclick()
+                    // choose number of child
+                    cy.get('#divChild #hrefIncChd').click()
 
-        cy.get('.selection').contains('Điểm khởi hành')
-            .click()
-        cy.get('.select2-results__option').contains('Tp. Hồ Chí Minh (SGN)')
-            .click()
-
-    })
-    it('Dynamic dropdown by Xpath', () => {
-        cy.xpath(".//span[@title='Điểm khởi hành']").click()
-        cy.xpath('//li[text()="Cần Thơ (VCA)"]').click()
+            })
+        })
     })
 })
